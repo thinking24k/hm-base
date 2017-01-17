c910cbeaf040a191cf3bb8c69459f98810efb2e4
thinking24k <553067271@qq.com>
2017-01-17 18:39:14 +0800

    Revert "项目导入"
    This reverts commit 3bd3bb1d0065201712659a25be192e7aef33c41d.

--- hm-admin/src/main/webapp/js/info/learner.js
+++
@@ -1,9 +0,0 @@
-require(["jquery", "utils", "pagination", "jq-validation", "datepicker"], function($, utils){
-	$(function(){
-		/**教育经历编辑表单***/
-		var $eduForm = $("#educationalForm");
-		$eduForm.find("input.datePicker").datepicker({
-			format: "yyyy-MM-dd"
-		});
-	});
-});
--- hm-builder/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-<%
-RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/pages/index.jsp");
-rd.forward(request, response);   
-%>
